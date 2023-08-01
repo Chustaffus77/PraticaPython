05c7126a2e93d435adff0e2759cdb767c8d0226f
Gustavo Baptista <chustaffus777@gmail.com>
2023-08-01 11:18:56 -0300

    2º exércio realizado com sucesso!

---
+++ ex_fixacao02.py
@@ -0,0 +1,12 @@
+nome  =  input ( "Qual seria o seu nome?" )
+
+valor = float( input("Coloque aqui o valor da conta a ser paga:R$"))
+
+des = valor *10/100
+
+vfinal = valor + des
+
+
+
+print ("Pelo excelente serviço do garçom, tivemos um acréscimo de 10% no valor final da conta. Agora o valor a ser pago é: R$",vfinal,".")
+print ( "Boa Noite," , nome , "!Espero que tenha aproveitado sua noite no restaurante Baratie!" )
